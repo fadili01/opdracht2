940e8eb2d51e117180c4daf5bf3cb521b70c94c9
MOHAMED FADILI <2166129@talnet.nl>
2023-05-16 09:25:05 +0200

    commit1

---
+++ hello.php
@@ -0,0 +1,26 @@
+<?php
+$variabele1 = 10;
+$variabele2 = 10;
+
+if($variabele2 == 10) {
+    echo "De twee waarden zijn gelijk";
+} else {
+echo "De twee waarden zijn ongelijk";
+}
+
+$variabele1 = 10;
+$variabele2 = 12;
+
+if($variabele2 != 10) {
+    echo "De twee waarden zijn ongelijk";
+}
+
+$variabele1 = 10;
+$variabele2 = 10;
+
+if($variabele2 == $variabele1)
+    echo "De twee waarden zijn gelijk";
+else {
+    echo "De twee waarden zijn ongelijk";
+}
+?>
